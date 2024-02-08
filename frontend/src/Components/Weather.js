8b562fb099c136ab14231d3a484977bf521796b1
albertohuerta <albertohuerta22@gmail.com>
2024-02-08 17:26:18 -0500

    user caches data on local storage

--- frontend/src/Components/Weather.js
+++ frontend/src/Components/Weather.js
@@ -7,19 +7,54 @@ const Weather = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await getWeather();
-        const dailyArray = response.data.timelines.daily;
-        const firstDayData = dailyArray[0];
-        const values = firstDayData.values;
-        const maxTempFahrenheit = values.temperatureMax;
-        const minTempFahrenheit = values.temperatureMin;
-        setCurrentMax(convertCelsiusToFahrenheit(maxTempFahrenheit));
-        setCurrentMin(convertCelsiusToFahrenheit(minTempFahrenheit));
-      } catch (error) {
-        console.log('Error fetching data', error);
+      // Define a unique key for the weather data cache
+      const cacheKey = 'weatherData';
+      // Try to get cached weather data
+      const cachedData = localStorage.getItem(cacheKey);
+      let weatherData;
+
+      if (cachedData) {
+        // Parse the cached JSON data
+        const parsedData = JSON.parse(cachedData);
+        // Check if the cache is still valid, assuming 1 hour validity
+        const isCacheValid =
+          new Date().getTime() - parsedData.timestamp < 3600000;
+
+        if (isCacheValid) {
+          weatherData = parsedData.data;
+        }
       }
+
+      if (!weatherData) {
+        try {
+          const response = await getWeather();
+          const dailyArray = response.data.timelines.daily;
+          const firstDayData = dailyArray[0];
+          const values = firstDayData.values;
+          weatherData = {
+            temperatureMax: values.temperatureMax,
+            temperatureMin: values.temperatureMin,
+          };
+
+          // Store the new data with a timestamp
+          localStorage.setItem(
+            cacheKey,
+            JSON.stringify({
+              timestamp: new Date().getTime(),
+              data: weatherData,
+            })
+          );
+        } catch (error) {
+          console.log('Error fetching data', error);
+          return;
+        }
+      }
+
+      // Set state with either cached or fetched data
+      setCurrentMax(convertCelsiusToFahrenheit(weatherData.temperatureMax));
+      setCurrentMin(convertCelsiusToFahrenheit(weatherData.temperatureMin));
     };
+
     fetchData();
   }, []);
 
